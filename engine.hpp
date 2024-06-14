40c84fa7aa23fa54136e4dee2fb3c02ff5a86e37
daunita <markpustog@gmail.com>
2024-06-14 11:54:01 +0500

    initial commit

---
+++ engine.hpp
@@ -0,0 +1,19 @@
+#ifndef INC_2G43S_ENGINE_HPP
+#define INC_2G43S_ENGINE_HPP
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+class engine {
+
+};
+
+
+#endif //INC_2G43S_ENGINE_HPP
